b2da6d65b32590d27a2b69a220398bd8e05a6e3f
Miguel Caldeira <miguelcaldeira2002@gmail.com>
2023-11-27 15:11:55 +0000

    fix

--- shell_FEM/Solution.py
+++ shell_FEM/Solution.py
@@ -128,7 +128,7 @@ def DinamicSolver(m:np.ndarray, c:np.ndarray, k:np.ndarray, f:np.ndarray, x_0:np
     while tk < t_final :
         
         #Force vector for current tk
-        # f = F(t = t_k)
+        # f = Carr_t(t = t_k)
 
         #Starting value [x_d2_(0)]
         x_0_d2 = np.linalg.inv(m) @ (f - (c @ x_0_d ) - (k @ x_0))

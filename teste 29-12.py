36991bf322965a3e98a17f6f4bab71fd97748311
Tiagojf07 <149185831+Tiagojf07@users.noreply.github.com>
2023-12-29 12:57:47 +0000

    teste de 29/12

---
+++ teste 29-12.py
@@ -0,0 +1,419 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import math
+import scipy as sp
+
+def Mesh_Properties():
+
+    # Number of Points to read
+    df_num_rows  = pd.read_excel('Livro1.xlsx', sheet_name = 'Input', usecols = ['NumRowsRead'], nrows = 1)
+    k            = int(df_num_rows.loc[0, 'NumRowsRead'])
+
+    #Materials to read
+    df_matcols  = pd.read_excel('Livro1.xlsx', sheet_name = 'Materials', usecols = [0], nrows = 1)
+    m           = int(df_matcols.iloc[0, 0])
+    matcols     = list(range(3, 3 + m))
+
+    # Reading the Input Data / Creating DataFrames
+    df_read     = ['Points','z','r','thi','Conditions','Material','Conditions1','Nn', 'Loading'] 
+    df          = pd.read_excel('Livro1.xlsx', sheet_name = 'Input', usecols = df_read, nrows = k) 
+                                                                                    
+    df_info     = pd.read_excel('Livro1.xlsx', sheet_name = 'Input', usecols = ['Discontinuity'], nrows = k)
+
+    df_mat      = pd.read_excel('Livro1.xlsx', sheet_name = 'Materials', usecols = matcols, nrows = 7)
+    
+    #print(df_mat)
+    
+    # Matriz com as propriedades do material
+    
+    material = np.array(df_mat.values)
+    
+    #print(material)
+
+    """
+    df.plot(x='r', y='z', marker='o', linestyle='-', color='k', label='Nós')
+    plt.gca().invert_yaxis()
+    plt.legend(loc='center left')
+    plt.show()
+    """
+
+    # Creates a DataFrame with the same columns, but with no values
+    empty_df        = pd.DataFrame([[None]*len(df_read)], columns = df_read)
+
+
+    #The first point can't be (0,0) due to mathematical issues
+    if df.loc[0, 'r'] == 0:
+
+        df.loc[0, 'r']  =  df.loc[0, 'r'] + 10**(-6) 
+
+
+    # Adding a point due to the discontinuity, regarding the geometry:
+    # helps on interpolation
+    i=0
+    while i < (len(df_info['Discontinuity'])):
+
+        if df_info.loc[i, 'Discontinuity' ] == 1:
+
+            df_add1 = pd.DataFrame  ({
+                'Points'        : [df.loc[i, 'Points'] + 0.0001],
+                'z'             : [df.loc[i, 'z'] + 10**(-6)],
+                'r'             : [df.loc[i, 'r']],
+                'thi'           : [df.loc[i+1, 'thi']],
+                'Conditions'    : [df.loc[i, 'Conditions1']],
+                'Nn'            : [df.loc[i, 'Nn']],
+                'Conditions1'   : [df.loc[i, 'Conditions1']],
+                'Material'      : [df.loc[i, 'Material']],
+                'Loading'       : [df.loc[i, 'Loading']]
+                                    })
+            
+            df_add2 = pd.DataFrame  ({
+                'Discontinuity' : [0]
+                                    })
+            
+            result1 = pd.concat([df.iloc[:i+1], df_add1, df.iloc[i+1:]], ignore_index=True)
+            df      = result1
+
+            result2 = pd.concat([df_info.iloc[:i+1], df_add2, df_info.iloc[i+1:]], ignore_index=True)
+            df_info = result2
+
+            df.loc[i,'Nn'] = 0
+
+        i += 1
+
+    # Adding empty rows, with the number of nodes necessary to have the number of elements specified by the user 
+    i = 0
+    while i < (len(df['Nn'])):
+
+        if df.loc[i, 'Nn' ] != 0:
+
+            j=0
+            while j < df.loc[i, 'Nn' ]:
+
+                position    = i + 1
+                result      = pd.concat([df.iloc[:position], empty_df, df.iloc[position:]], ignore_index=True)
+                df          = result
+
+                j += 1
+                
+        i +=1
+
+    # Complement information for the New Nodes
+    i = 1
+    while i < (len(df['Points'])):
+
+        if pd.isna(df.loc[i, 'Points']): #If the specified value is NaN
+
+            df.loc[i, 'Points']         = df.loc[i-1, 'Points'] + 0.0001
+            df.loc[i, 'Conditions']     = df.loc[i-1, 'Conditions1']
+            df.loc[i,'Conditions1']     = df.loc[i-1, 'Conditions1']
+            df.loc[i,'Material']        = df.loc[i-1, 'Material']
+        i += 1  
+
+
+    # Interpolation Linear Type
+    columns_interpolate     = ['z', 'r', 'thi', 'Loading']
+    df[columns_interpolate] = df[columns_interpolate].interpolate(method='linear')
+    df.loc[len(df)-1, 'thi'] = np.nan 
+
+    #print(df)
+
+
+    # Matriz com as coordenadas dos pontos / Malha
+
+    mesh = np.array(df[['z','r']].values)
+
+    #print(mesh)
+    
+
+
+
+    # Defining each condition possible for each nodes
+    condition0 = [0,0,0]
+    condition1 = [1,0,0]
+    condition2 = [0,1,0]
+    condition3 = [0,0,1]
+    condition4 = [1,1,0]
+    condition5 = [1,0,1]
+    condition6 = [0,1,1]
+    condition7 = [1,1,1]
+    conditions = [condition0, condition1, condition2, condition3, condition4, condition5, condition6, condition7]
+
+    # Creating the Boundary Conditions DataFrame as empty
+    Boundary_Conditions =   {   'Point'         : [],
+                                'Displacement'  : [],
+                                'Value'         : []
+                            }
+    Boundary_Conditions = pd.DataFrame(Boundary_Conditions)
+
+    # Adding Rows without information on column 'Value' 
+    i = 0
+    while i < (len(df['Points'])):
+        empty_df1 = {   'Point'         : [df.loc[i, 'Points' ], df.loc[i, 'Points' ], df.loc[i, 'Points' ]],
+                        'Displacement'  : ['v','w','theta'],
+                        'Value'         : [np.nan, np.nan, np.nan]
+                    }
+        empty_df1               = pd.DataFrame(empty_df1)
+        result                  = pd.concat([Boundary_Conditions, empty_df1], ignore_index=True)
+        Boundary_Conditions     = result
+        i +=1
+
+    # Filling the Boundary Conditions with the respective values
+    j=0
+    for i in range(0, len(Boundary_Conditions), 3):
+        condition_index = int(df.loc[j, 'Conditions'])
+        
+        if 0 <= condition_index < len(conditions):
+            Boundary_Conditions.loc[i:i+2, 'Value'] = conditions[condition_index]
+        
+        j += 1
+
+    #print(Boundary_Conditions)
+
+
+
+    # Nome do Vetor construído com os dados da coluna "Value" : u_DOF
+
+    u_DOF = np.array(Boundary_Conditions["Value"].values)
+
+    u_DOF = u_DOF.reshape((-1, 1))
+    
+    #print(u_DOF)
+
+    
+
+
+
+
+    # Creation of DataFrame regarding the elements
+    vpe =   {   'Node_i'    : [],
+                'phi'       : [],
+                'h'         : [],
+                'thi'       : [],
+                'mat'       : []
+            }
+    vpe = pd.DataFrame(vpe)
+
+    # Adding the empty rows necessary
+    for i in range(len(df)-1):
+        add =   { 'Node_i'  : [df.loc[i, 'r']],
+                'phi'       : [np.nan],
+                'h'         : [np.nan],
+                'thi'       : [df.loc[i, 'thi']],
+                'mat'       : [df.loc[i, 'Material']]
+                }
+        add     = pd.DataFrame(add)
+        result  = pd.concat([vpe, add], ignore_index=True)
+        vpe     = result
+
+
+
+    # Adding the other information
+    for i in range(len(df)-1):
+        vpe.loc[i, 'h'] = math.sqrt( (df.loc[i+1, 'z'] - df.loc[i, 'z'])**2 + (df.loc[i+1, 'r']-df.loc[i, 'r'])**2 )
+
+        if (df.loc[i+1, 'z'] - df.loc[i, 'z']) != 0:
+            vpe.loc[i, 'phi'] = math.atan( (df.loc[i+1, 'r'] - df.loc[i, 'r']) / (df.loc[i+1, 'z'] - df.loc[i, 'z']) )
+        else:
+            if df.loc[i+1, 'r'] > df.loc[i, 'r']:
+                vpe.loc[i, 'phi'] = math.pi/2
+
+            elif df.loc[i+1, 'r'] < df.loc[i, 'r']:
+                vpe.loc[i, 'phi'] = -(math.pi/2)
+
+    #print(vpe)
+
+    vpe = np.array(vpe.values)
+    
+    #print(vpe)
+
+    
+    return mesh, u_DOF, vpe, material
+
+
+
+
+"""
+ #Graphic of the points
+df.plot(x='r', y='z', marker='o', linestyle='-', color='k', label='Nós')
+plt.gca().invert_yaxis()
+plt.legend(loc='center left')
+plt.show()
+"""
+
+#PARTE ALFAGEM
+
+
+def Bi(s1:float, index:int, r:float, vpe) -> np.ndarray:
+    phi = vpe[index, 1]
+    h = vpe[index, 2]
+    sen_phi = np.sin(phi)
+    cos_phi = np.cos(phi)
+
+    return np.array([[-1/h, 0, 0],
+                     [(1-s1)*sen_phi/r, (1-3*s1**2+2*s1**3)*cos_phi/r, h*s1*(1-2*s1+s1**2)*cos_phi/r],
+                     [0, 6*(1-2*s1)/(h**2), 2*(2-3*s1)/h],
+                     [0, 6*s1*(1-s1)*sen_phi/(r*h), (-1+4*s1-3*s1**2)*sen_phi/r]])
+
+def Bj(s1:float, index:int, r:float, vpe) -> np.ndarray:
+    phi = vpe[index, 1]
+    h = vpe[index, 2]
+    sen_phi = np.sin(phi)
+    cos_phi = np.cos(phi)
+
+    return np.array([[1/h, 0, 0],
+                     [s1*sen_phi/r, (s1**2)*(3-2*s1)*cos_phi/r, h*(s1**2)*(-1+s1)*cos_phi/r],
+                     [0, -6*(1-2*s1)/(h**2), 2*(1-3*s1)/h],
+                     [0, -6*s1*(1-s1)*sen_phi/(r*h), s1*(2-3*s1)*sen_phi/r]])
+
+def elastM(index:int, vpe, mat) -> np.ndarray:
+    E = mat[int(vpe[index, 4]), 1] # mat must have more than one material so that the array is 2D by default
+    t = vpe[index, 3]
+    upsilon = mat[int(vpe[index, 4]), 2]
+    D = (E*t)/(1-upsilon**2)*np.array([[1,upsilon, 0, 0],[upsilon, 1, 0, 0],[0, 0, (t**2/12), upsilon*(t**2/12)],[0, 0, upsilon*(t**2/12), (t**2/12)]])
+                                                                                                                                    
+    return D
+
+def transM(phi) -> np.ndarray:
+    T = np.array([[np.cos(phi), np.sin(phi), 0],[-np.sin(phi), np.cos(phi), 0],[0, 0, 1]])
+    return T
+
+def Bmatrix(s1:float, index:int, r:float, phi:float, vpe) -> np.ndarray:
+    T = transM(phi)
+    return np.hstack((Bi(s1,index,r, vpe)@T,Bj(s1,index,r, vpe)@T))
+
+def Pbar(s1:float, h:float) -> np.ndarray:
+    return np.array([[1-s1, 0, 0, s1, 0, 0],
+                     [0, 1-3*s1**2+2*s1**3, s1*(1-2*s1+s1**2)*h, 0, (s1**2)*(3-2*s1), (s1**2)*(s1-1)*h]])
+
+def Pmatrix(s1:float, index:int, phi:float, vpe) -> np.ndarray:
+    T = transM(phi)
+    h = vpe[index, 2]
+    P = Pbar(s1, h)
+    #print(P, '\n')
+    Pi, Pj = np.hsplit(P, 2)
+    #print(Pi)
+    #print(Pj)
+    return np.hstack((Pi@T, Pj@T))
+
+def Kestacked(ne:int, vpe, mat, ni:int, simpson=True) -> np.ndarray: # Incoeherent integration results
+    kes = np.empty((6,6,ne), dtype=float)
+    for i in range(0, ne):
+        phi = vpe[i, 1]
+        ri = vpe[i, 0]
+        h = vpe[i, 2]
+        D = elastM(i, vpe, mat)
+        if simpson:
+            I = np.empty((6, 6, ni+1), dtype=float)
+            for j, s1 in enumerate(np.linspace(0,1,ni+1)):
+                r = ri + s1*h*np.sin(phi)
+                B = Bmatrix(s1,i,r,phi, vpe)
+                I[:,:,j] = B.T@D@B*(r)
+
+            ke = 2*np.pi*h*sp.integrate.simpson(I, x=None, dx=h/ni, axis=-1)
+            #print(ke, '\n')
+        else:
+            s1_ = lambda s: (s+1)/2
+            r = lambda s: ri + s1_(s)*h*np.sin(phi)
+            integrand = lambda s: Bmatrix(s1_(s),i,r(s),phi, vpe).T@D@Bmatrix(s1_(s),i,r(s),phi, vpe)*(r(s))
+            ke = 2*np.pi*h*((5/9)*integrand(-np.sqrt(3/5))+(8/9)*integrand(0)+(5/9)*integrand(np.sqrt(3/5)))
+            #print(ke)
+        kes[:,:,i] = ke
+    return kes
+
+def k_global(ne:int, vpe, mat, ni=1200, sparse=False) -> np.ndarray:
+    kes = Kestacked(ne, vpe, mat, ni)
+    if sparse:
+        row = []
+        column = []
+        data = []
+        for i in range(0, ne):
+            for j in range(0,6):
+                for k in range(0,6):
+                    row.append(3*i+j)
+                    column.append(3*i+k)
+                    data.append(kes[j, k, i])
+        #print(row)
+        #print(column)
+        #print(data)
+        k_globalM = sp.sparse.bsr_array((data, (row, column)), shape=(3*(ne+1), 3*(ne+1)))#.toarray()     
+    else:
+        k_globalM = np.zeros((3*(ne+1), 3*(ne+1)), dtype='float64')
+        for i in range(0,ne):
+            k_globalM[3*i:3*i+6,3*i:3*i+6] = k_globalM[3*i:3*i+6,3*i:3*i+6] + kes[:,:,i]
+    
+    return k_globalM
+    #print(f'Element {i+1}')
+    #for j in range(0, 3*(ne+1)):
+    #    for k in range(0, 3*(ne+1)):
+    #        print(f'{k_globalM[j, k]:.2}', end='   ')
+    #    print()
+    #print('\n\n')
+
+    #print(sparse)
+ 
+
+def calculate_strains_stresses(displacements, vpe, mat):
+    num_nodes = len(displacements)/3
+    num_elements = len(vpe)
+
+    strains = np.zeros((num_nodes, 4))  # Matriz para armazenar as deformações de cada nó (epsilon_s, epsilon_theta, chi_s, chi_theta)
+    for i in range(num_elements):
+        R = vpe[i,0]
+        phi = vpe[i,1]
+        B = Bmatrix(0, i, R, phi, vpe)  # Obtém a matriz B para s1 = 0 
+        strains[i,:] += B @ displacements[3*i:3*i+6]  # Multiplica deslocamentos pelos valores da matriz B exceto o ultimo
+    i = num_elements - 1
+    h = vpe[i, 2]
+    B = Bmatrix(1, i, R +h*np.sin(phi), phi, vpe)  # Obtém a matriz B para s1 = 1
+    strains[i+1,:] += B @ displacements[3*i:3*i+6]  # Multiplica deslocamentos pelos valores da matriz B obtendo a ultima extensao
+
+    forças_N = np.zeros((num_nodes, 4))
+    for i in range(num_elements):
+        D = elastM(i, vpe, mat)
+        forças_N[i,:] += D @ (strains[i,:].T)
+    forças_N[i+1,:] += D @ (strains[i+1,:].T)          
+
+    tensoes_N = np.zeros((num_nodes, 4))
+    for i in range(num_elements):
+        t = vpe[i, 3]
+        sigma_sd = forças_N[i,0]/t -  6*forças_N[i,2]/t**2
+        sigma_sf = forças_N[i,0]/t +  6*forças_N[i,2]/t**2
+        sigma_td = forças_N[i,1]/t -  6*forças_N[i,3]/t**2
+        sigma_tf = forças_N[i,1]/t +  6*forças_N[i,3]/t**2
+        tensoes_N[i, :] = [sigma_sd, sigma_td, sigma_sf, sigma_tf]
+    i += 1
+    sigma_sd = forças_N[i,0]/t - 6*forças_N[i,2]/t**2
+    sigma_sf = forças_N[i,0]/t + 6*forças_N[i,2]/t**2
+    sigma_td = forças_N[i,1]/t - 6*forças_N[i,3]/t**2
+    sigma_tf = forças_N[i,1]/t +     6*forças_N[i,3]/t**2
+    tensoes_N[i, :] = [sigma_sd, sigma_td, sigma_sf, sigma_tf]
+    return strains, tensoes_N
+
+def tensões_VM(displacements, vpe, mat):
+    num_nodes = len(displacements)/3
+    num_elements = len(vpe)
+    tensoes_N = calculate_strains_stresses(displacements, vpe, mat)
+    VM = np.zeros((num_nodes, 2))
+    for i in range(num_elements):
+        VM[i,0] = np.sqrt(tensoes_N[i,0]**2 -tensoes_N[i,0]*tensoes_N[i,2] + tensoes_N[i,2]**2)
+        VM[i,1] = np.sqrt(tensoes_N[i,1]**2 -tensoes_N[i,1]*tensoes_N[i,3] + tensoes_N[i,3]**2)
+    i = num_elements + 1
+    VM[i,0] = np.sqrt(tensoes_N[i,0]**2 -tensoes_N[i,0]*tensoes_N[i,2] + tensoes_N[i,2]**2)
+    VM[i,1] = np.sqrt(tensoes_N[i,1]**2 -tensoes_N[i,1]*tensoes_N[i,3] + tensoes_N[i,3]**2)
+    return VM
+
+def FS(displacements ,VM, material, vpe):
+    VM = tensões_VM(displacements, vpe, material)
+    FS = np.zeros(len(VM))
+    for i, VM in enumerate(VM):
+        VM_max = max(VM)
+        FS[i] = VM_max /material[i, ] 
+    return FS
+
+
+mesh, u_DOF, vpe, material = Mesh_Properties()
+
+k = k_global(len(vpe), vpe, material)
+
+print("matriz K \n", k)

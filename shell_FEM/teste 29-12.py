6c8f435dd4da427c6ee83d102841caf80af8432c
Migue1c <148803895+Migue1c@users.noreply.github.com>
2023-12-30 18:42:31 +0000

    1

--- shell_FEM/teste 29-12.py
+++ shell_FEM/teste 29-12.py
@@ -456,7 +456,6 @@ def FS(displacements, vpe, mat, VM, tensões_N):     #FSy - deformação plastic
 
 #CARREGAMENTO
 
-
 def pressao(w,nev,nl,press_est,nn):
     pressure = np.zeros(nn)
     for i in range(0, nl):
@@ -509,6 +508,66 @@ def Carr_t(loading,t,t_col,P_col,press_max):
     loading=loading*P_t
     #print(loading)
     return loading
+
+
+
+#MODAL
+
+def Mestacked(ne:int, vpe, mat, ni:int, simpson=True) -> np.ndarray:
+    mes = np.empty((6,6,ne), dtype=float)
+    for i in range(0, ne):
+        rho = mat[int(vpe[i, 4]), 0] # Specific mass for the material in the i-th element
+        t = vpe[i,3]
+        h = vpe[i, 2]
+        phi = vpe[i, 1]
+        ri = vpe[i, 0]
+        if simpson:
+            I = np.empty((6, 6, ni+1), dtype=float)
+            for j, s1 in enumerate(np.linspace(0,1,ni+1) ):
+                r = ri + s1*h*np.sin(phi)
+                P = Pmatrix(s1,i,phi, vpe)
+                I[:,:,j] = (r)*P.T@P
+
+            me = rho*t*2*sp.pi*h*sp.integrate.simpson(I, x=None, dx=h/ni, axis=-1)
+        #print('The mass matrix is:\n', me)
+        mes[:,:,i] = me
+    return mes
+
+def m_global(ne:int, vpe, mat, ni=1200, sparse=False) -> np.ndarray:
+    global m_globalM
+    mes = Mestacked(ne, vpe, mat, ni)
+    if sparse:
+        row = []
+        column = []
+        data = []
+        for i in range(0, ne):
+            for j in range(0,6):
+                for k in range(0,6):
+                    row.append(3*i+j)
+                    column.append(3*i+k)
+                    data.append(mes[j, k, i])
+        #print(row)
+        #print(column)
+        #print(data)
+        m_globalM = sp.sparse.bsr_array((data, (row, column)), shape=(3*(ne+1), 3*(ne+1)))#.toarray()     
+    else:
+        m_globalM = np.zeros((3*(ne+1), 3*(ne+1)), dtype='float64')
+        for i in range(0,ne):
+            m_globalM[3*i:3*i+6,3*i:3*i+6] = m_globalM[3*i:3*i+6,3*i:3*i+6] + mes[:,:,i]
+    return m_globalM
+
+def modal_analysis(ne, vpe, u_DOF, mat, ni=1200, sparse=False, is_called_from_dynamic=False):
+    k_globalM = k_global(ne, vpe, mat, ni, sparse)
+    m_globalM = m_global(ne, vpe, mat, ni, sparse)
+    if is_called_from_dynamic:
+        natfreq = (np.sqrt(sp.linalg.eigh(k_globalM, m_globalM, eigvals_only=True))[0:2])/(2*np.pi)
+        return natfreq[0], natfreq[1], m_globalM, k_globalM
+    else:
+        ModalSolver(k_global,m_global, u_DOF)
+    output = np.array[eig_vals,eig_vect]
+
+
+
 
 
 
